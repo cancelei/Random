195f35498248cbaa0830ae4abee47f84e15a28e3
cancelei <glauber.bannwart@gmail.com>
2024-11-04 12:42:28 -0300

    feat/refactor product data

--- vanilla_ruby_stock_control_poo/init.rb
+++ vanilla_ruby_stock_control_poo/init.rb
@@ -4,9 +4,5 @@ require_relative 'models/product'
 require_relative 'services/product_service'
 require_relative 'display/menu'
 
-# Add some sample products
-ProductService.add_product(Product.new(id: 1, name: "Product 1", description: "Description 1", price: 10, quantity: 100))
-ProductService.add_product(Product.new(id: 2, name: "Product 2", description: "Description 2", price: 20, quantity: 200))
-
 menu = Menu.new
 menu.display_menu
